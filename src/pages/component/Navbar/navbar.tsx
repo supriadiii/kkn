b175c19117bde884cf70c489b8ce0e9140c01a9e
supriadi <supriadiadi1001@gmail.com>
2023-08-09 02:23:10 +0700

    update:

--- src/pages/component/Navbar/navbar.tsx
+++ src/pages/component/Navbar/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar({ fixed = true, scrolled }: any) {
     setShowMenu(!showMenu);
   };
 
-  const navbarClasses = `w-full py-10 px-10 gap-20 flex items-center shadow-md absolute top-0 left-0 right-0 sm:px-10 justify-${
+  const navbarClasses = `w-full py-10 px-10 gap-20 bg-white flex items-center shadow-md absolute top-0 left-0 right-0 sm:px-10 justify-${
     fixed ? "sticky" : ""
   } ${scrolled ? "bg-white" : ""}`;
 
@@ -17,50 +17,108 @@ export default function Navbar({ fixed = true, scrolled }: any) {
 
   return (
     <div className={navbarClasses}>
-      <Image
-        src="/asset/logo.png"
-        alt="Image Description"
-        width={175}
-        height={20}
-      />
+      <div className="flex justify-between w-full ">
+        <div className="flex items-center  gap-16">
+          <Image
+            src="/asset/logo.png"
+            alt="Image Description"
+            width={175}
+            height={20}
+          />
 
-      <Link
-        href="/"
-        className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
-      >
-        Beranda
-      </Link>
-      <Link
-        href="/visimisi"
-        className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
-      >
-        Visi Dan Misi
-      </Link>
-      <Link
-        href="/struktur"
-        className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
-      >
-        Struktur Organisasi
-      </Link>
-      <Link
-        href="/informasi"
-        className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
-      >
-        Informasi
-      </Link>
-      <Link
-        href="/informasi"
-        className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
-      >
-        Kontak
-      </Link>
+          <Link
+            href="/"
+            className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
+          >
+            Beranda
+          </Link>
+          <Link
+            href="/visimisi"
+            className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
+          >
+            Visi Dan Misi
+          </Link>
+          <Link
+            href="/struktur"
+            className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
+          >
+            Struktur Organisasi
+          </Link>
+          <Link
+            href="/informasi"
+            className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
+          >
+            Informasi
+          </Link>
+          <Link
+            href="/kontak"
+            className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
+          >
+            Kontak
+          </Link>
 
-      <button
-        className="text-gray-600 hover:border-b-2 hover:border-black sm:hidden"
-        onClick={toggleMenu}
-      >
-        Informasi
-      </button>
+          <button
+            className="text-gray-600 hover:border-b-2 hover:border-black sm:hidden"
+            onClick={toggleMenu}
+          >
+            Informasi
+          </button>
+        </div>
+        <div className="flex justify-center">
+          <Link
+            href="/kontak"
+            className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-8 h-8 justify-center "
+            >
+              <path
+                xmlns="http://www.w3.org/2000/svg"
+                d="M20 12.05C19.9813 10.5255 19.5273 9.03809 18.6915 7.76295C17.8557 6.48781 16.673 5.47804 15.2826 4.85257C13.8921 4.2271 12.3519 4.01198 10.8433 4.23253C9.33473 4.45309 7.92057 5.10013 6.7674 6.09748C5.61422 7.09482 4.77005 8.40092 4.3343 9.86195C3.89856 11.323 3.88938 12.8781 4.30786 14.3442C4.72634 15.8103 5.55504 17.1262 6.69637 18.1371C7.83769 19.148 9.24412 19.8117 10.75 20.05V14.38H8.75001V12.05H10.75V10.28C10.7037 9.86846 10.7483 9.45175 10.8807 9.05931C11.0131 8.66687 11.23 8.30827 11.5161 8.00882C11.8022 7.70936 12.1505 7.47635 12.5365 7.32624C12.9225 7.17612 13.3368 7.11255 13.75 7.14003C14.3498 7.14824 14.9482 7.20173 15.54 7.30003V9.30003H14.54C14.3676 9.27828 14.1924 9.29556 14.0276 9.35059C13.8627 9.40562 13.7123 9.49699 13.5875 9.61795C13.4627 9.73891 13.3667 9.88637 13.3066 10.0494C13.2464 10.2125 13.2237 10.387 13.24 10.56V12.07H15.46L15.1 14.4H13.25V20C15.1399 19.7011 16.8601 18.7347 18.0985 17.2761C19.3369 15.8175 20.0115 13.9634 20 12.05Z"
+                fill="#000000"
+              />
+            </svg>
+          </Link>
+          <Link
+            href="/kontak"
+            className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-10 h-10 justify-center "
+            >
+              <path
+                xmlns="http://www.w3.org/2000/svg"
+                d="M20 12.05C19.9813 10.5255 19.5273 9.03809 18.6915 7.76295C17.8557 6.48781 16.673 5.47804 15.2826 4.85257C13.8921 4.2271 12.3519 4.01198 10.8433 4.23253C9.33473 4.45309 7.92057 5.10013 6.7674 6.09748C5.61422 7.09482 4.77005 8.40092 4.3343 9.86195C3.89856 11.323 3.88938 12.8781 4.30786 14.3442C4.72634 15.8103 5.55504 17.1262 6.69637 18.1371C7.83769 19.148 9.24412 19.8117 10.75 20.05V14.38H8.75001V12.05H10.75V10.28C10.7037 9.86846 10.7483 9.45175 10.8807 9.05931C11.0131 8.66687 11.23 8.30827 11.5161 8.00882C11.8022 7.70936 12.1505 7.47635 12.5365 7.32624C12.9225 7.17612 13.3368 7.11255 13.75 7.14003C14.3498 7.14824 14.9482 7.20173 15.54 7.30003V9.30003H14.54C14.3676 9.27828 14.1924 9.29556 14.0276 9.35059C13.8627 9.40562 13.7123 9.49699 13.5875 9.61795C13.4627 9.73891 13.3667 9.88637 13.3066 10.0494C13.2464 10.2125 13.2237 10.387 13.24 10.56V12.07H15.46L15.1 14.4H13.25V20C15.1399 19.7011 16.8601 18.7347 18.0985 17.2761C19.3369 15.8175 20.0115 13.9634 20 12.05Z"
+                fill="#000000"
+              />
+            </svg>
+          </Link>
+          <Link
+            href="/kontak"
+            className="text-gray-600 hover:border-b-2 hover:border-black hidden sm:block"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-10 h-10 justify-center "
+              width="8px"
+              height="8px"
+            >
+              <path
+                xmlns="http://www.w3.org/2000/svg"
+                d="M160,128a32,32,0,1,1-32-32A32.03667,32.03667,0,0,1,160,128Zm68-44v88a56.06353,56.06353,0,0,1-56,56H84a56.06353,56.06353,0,0,1-56-56V84A56.06353,56.06353,0,0,1,84,28h88A56.06353,56.06353,0,0,1,228,84Zm-52,44a48,48,0,1,0-48,48A48.05436,48.05436,0,0,0,176,128Zm16-52a12,12,0,1,0-12,12A12,12,0,0,0,192,76Z"
+              />
+            </svg>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/pages/kontak/index.tsx
@@ -0,0 +1,132 @@
+import Image from "next/image";
+import Navbar from "../component/Navbar/navbar";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import DetailBerita from "../detailBerita/index";
+export default function Kontak() {
+  const [scrolled, setScrolled] = useState(false);
+  const router = useRouter();
+  const data = router.query.data;
+
+  // Parse the data back to an object
+  const beritaData =
+    typeof data === "string"
+      ? JSON.parse(data)
+      : { judulBerita: "", tanggalTerbit: "", imageUrl: "" };
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const scrollThreshold = 100;
+
+    if (scrollPosition > scrollThreshold) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+  console.log("ini beritaData", beritaData);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <>
+      <Navbar fixed={scrolled} scrolled={scrolled} />
+
+      <div className="w-full h-full  bg-slate-100">
+        <div className="flex justify-center text-center mt-20 py-40  px-48 bg-black  text-white">
+          <div>
+            <p className="text-4xl font-bold">Kontak Kami</p>
+          </div>
+        </div>
+
+        <div className="flex justify-center gap-10 px-32 my-20 mx-auto ">
+          <div className="justify-center w-[38rem] bg-white item-center text-center py-20 px-40  rounded-2xl">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-28 h-28 justify-center mx-auto mb-8"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                clip-rule="evenodd"
+              />
+            </svg>
+
+            <p className=" text-xl">+62808xxxxxxxxx</p>
+          </div>
+          <div className="justify-center w-[38rem] bg-white item-center text-center py-20 px-40  rounded-2xl">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-28 h-28 justify-center mx-auto mb-8"
+            >
+              <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+              <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+            </svg>
+
+            <p className=" text-xl">ANAONIMUS@gmail.com</p>
+          </div>
+        </div>
+        <div className="flex justify-center gap-10 px-32 my-20 mx-auto h-96 ">
+          <div className="justify-center w-[38rem] bg-white item-center text-center py-20 px-40  rounded-2xl ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-28 h-28 justify-center mx-auto mb-8"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
+                clip-rule="evenodd"
+              />
+            </svg>
+
+            <p className=" text-xl">
+              Tebing Tinggi, Tanjung Beringin, Serdang Bedagai Regency, North
+              Sumatra 20996
+            </p>
+          </div>
+        </div>
+        <Image
+          src="/asset/maps.png"
+          alt="Image Description"
+          width={1000}
+          height={800}
+          className="rounded-[40px] mx-auto my-2"
+        />
+        <div className="h-20 border-t-4 mt-20"></div>
+
+        {/* <div className="  px-48 ">
+          {beritaData.map((member: any, index: number) => (
+            <Link href={`/`} key={index} className="hover:text-red-500">
+              <div className="flex px-16 py-10 gap-10 mb-16 items-center bg-white rounded-2xl">
+                <Image
+                  src="/asset/background.jpg"
+                  alt="Image Description"
+                  width={302}
+                  height={302}
+                  className="rounded-[50px]  my-2"
+                />
+                <div>
+                  <p className="text-4xl font-[800] mb-4">
+                    {member.judulBerita}
+                  </p>
+                  <p>{member.tanggalTerbit}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div> */}
+      </div>
+    </>
+  );
+}
